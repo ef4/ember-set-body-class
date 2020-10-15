bbf13072918bd3c3e19d690bd5d812e459bedc96
Tobias Bieniek <tobias@bieniek.cloud>
2020-10-15 12:43:19 +0200

    tests: Use `setApplication()` instead of `setResolver()`

--- tests/test-helper.js
+++ tests/test-helper.js
@@ -1,6 +1,7 @@
-import resolver from './helpers/resolver';
-import { setResolver } from '@ember/test-helpers';
+import Application from 'dummy/app';
+import config from 'dummy/config/environment';
+import { setApplication } from '@ember/test-helpers';
 import { start } from 'ember-qunit';
 
-setResolver(resolver);
+setApplication(Application.create(config.APP));
 start();
