d52611043f2514b70c56a5ba409259b7cf05cee4
Tobias Bieniek <tobias@bieniek.cloud>
2020-10-15 12:21:23 +0200

    ember-try: Use `ember-source-channel-url` for release channels

--- config/ember-try.js
+++ config/ember-try.js
@@ -1,92 +1,67 @@
 /* eslint-env node */
-module.exports = {
-  useYarn: true,
-  scenarios: [
-    {
-      name: 'ember-3.10',
-      npm: {
-        devDependencies: {
-          'ember-source': '~3.10.0',
+
+const getChannelURL = require('ember-source-channel-url');
+
+module.exports = async function () {
+  return {
+    useYarn: true,
+    scenarios: [
+      {
+        name: 'ember-3.10',
+        npm: {
+          devDependencies: {
+            'ember-source': '~3.10.0',
+          },
         },
       },
-    },
-    {
-      name: 'ember-lts-3.12',
-      npm: {
-        devDependencies: {
-          'ember-source': '~3.12.0',
+      {
+        name: 'ember-lts-3.12',
+        npm: {
+          devDependencies: {
+            'ember-source': '~3.12.0',
+          },
         },
       },
-    },
-    {
-      name: 'ember-lts-3.16',
-      npm: {
-        devDependencies: {
-          'ember-source': '~3.16.0',
+      {
+        name: 'ember-lts-3.16',
+        npm: {
+          devDependencies: {
+            'ember-source': '~3.16.0',
+          },
         },
       },
-    },
-    {
-      name: 'ember-lts-3.20',
-      npm: {
-        devDependencies: {
-          'ember-source': '~3.20.0',
+      {
+        name: 'ember-lts-3.20',
+        npm: {
+          devDependencies: {
+            'ember-source': '~3.20.0',
+          },
         },
       },
-    },
-    {
-      name: 'ember-release',
-      bower: {
-        dependencies: {
-          ember: 'components/ember#release',
-        },
-        resolutions: {
-          ember: 'release',
+      {
+        name: 'ember-release',
+        npm: {
+          devDependencies: {
+            'ember-source': await getChannelURL('release'),
+          },
         },
       },
-      npm: {
-        devDependencies: {
-          'ember-source': null,
+      {
+        name: 'ember-beta',
+        npm: {
+          devDependencies: {
+            'ember-source': await getChannelURL('beta'),
+          },
         },
       },
-    },
-    {
-      name: 'ember-beta',
-      bower: {
-        dependencies: {
-          ember: 'components/ember#beta',
-        },
-        resolutions: {
-          ember: 'beta',
+      {
+        name: 'ember-canary',
+        npm: {
+          devDependencies: {
+            'ember-source': await getChannelURL('canary'),
+          },
         },
       },
-      npm: {
-        devDependencies: {
-          'ember-source': null,
-        },
-      },
-    },
-    {
-      name: 'ember-canary',
-      bower: {
-        dependencies: {
-          ember: 'components/ember#canary',
-        },
-        resolutions: {
-          ember: 'canary',
-        },
-      },
-      npm: {
-        devDependencies: {
-          'ember-source': null,
-        },
-      },
-    },
-    {
-      name: 'ember-default',
-      npm: {
-        devDependencies: {},
-      },
-    },
-  ],
+    ],
+  };
 };
