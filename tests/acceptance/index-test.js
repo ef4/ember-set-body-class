700d9a85843cbb592fd5e858d62f70e939b0d743
Tobias Bieniek <tobias@bieniek.cloud>
2020-10-16 13:31:09 +0200

    tests/acceptance: Add `unrelated-class` assertions

--- tests/acceptance/index-test.js
+++ tests/acceptance/index-test.js
@@ -7,16 +7,24 @@ module('Acceptance | index', function (hooks) {
 
   test('updates body class', async function (assert) {
     await visit('/');
-    assert.dom(document.body).hasClass('red-text');
+    assert.dom(document.body).hasClass('red-text').hasClass('unrelated-class');
 
     await click('input[name=blueBackground]');
-    assert.dom(document.body).hasClass('red-text').hasClass('blue-background');
+    assert
+      .dom(document.body)
+      .hasClass('red-text')
+      .hasClass('blue-background')
+      .hasClass('unrelated-class');
 
     await click('input[name=redText]');
-    assert.dom(document.body).doesNotHaveClass('red-text').hasClass('blue-background');
+    assert
+      .dom(document.body)
+      .doesNotHaveClass('red-text')
+      .hasClass('blue-background')
+      .hasClass('unrelated-class');
 
     await fillIn('input[name=dynamicClassName]', 'blue-background');
     await click('input[name=blueBackground]');
-    assert.dom(document.body).hasClass('blue-background');
+    assert.dom(document.body).hasClass('blue-background').hasClass('unrelated-class');
   });
 });
