f44509c46a35ad885f03d77239cd22f09d0ef92c
Tobias Bieniek <tobias@bieniek.cloud>
2020-10-16 13:35:00 +0200

    tests/acceptance: Add render output assertions

--- tests/acceptance/index-test.js
+++ tests/acceptance/index-test.js
@@ -9,6 +9,11 @@ module('Acceptance | index', function (hooks) {
     await visit('/');
     assert.dom(document.body).hasClass('red-text').hasClass('unrelated-class');
 
+    // ensure that `{{set-body-class}}` does not render anything to the DOM
+    let selectBox = this.element.querySelector('[data-test-select]');
+    let app = selectBox.parentElement;
+    assert.equal(app.firstElementChild.tagName, 'LABEL');
+
     await click('input[name=blueBackground]');
     assert
       .dom(document.body)
